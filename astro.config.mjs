5f2ec75956fff84d086b3300898b53edbcafb00c
Charles <charles.xue@inclusion-factory.com>
2026-06-25 22:20:25 +0800

    ownership update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://charlezxue.github.io',
+  site: 'https://xinran1005.github.io',
 });
